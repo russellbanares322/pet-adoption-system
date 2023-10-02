7a253757277620a2fdaf02bc13927eadba5bf90b
russellbanares322 <banaresrussell11@gmail.com>
2023-10-02 21:32:13 +0800

    feat: build app for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>Hello</>;
+  return (
+    <div className="font-karla">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque
+      et nobis nemo ex nesciunt at quasi dolor eius tempora odit debitis natus
+      molestias rerum numquam dolorum illo, dolore voluptates.
+    </div>
+  );
 }
 
 export default App;
