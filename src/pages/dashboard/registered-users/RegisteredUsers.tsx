8f2f6d4bfb1f0fd145fdfd3413ab3e1d7c2ef6c0
russellbanares322 <banaresrussell11@gmail.com>
2023-10-31 10:21:02 +0800

    feat: instantiate table with the list of all registered users in dashboard page

--- src/pages/dashboard/registered-users/RegisteredUsers.tsx
+++ src/pages/dashboard/registered-users/RegisteredUsers.tsx
@@ -1,5 +1,38 @@
+import { Table } from "antd";
+import moment from "moment";
+import { useFetchUsers } from "../../../api/users/users";
+
 const RegisteredUsers = () => {
-  return <div>RegisteredUsers</div>;
+  const { data: registeredUsersData, isLoading } = useFetchUsers();
+
+  const dataSource = registeredUsersData?.map((user, index) => ({
+    id: index + 1,
+    email: user?.email,
+    dateCreated: moment(user?.dateCreated.toDate()).format("LLL"),
+  }));
+
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Email Address",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Date Created",
+      dataIndex: "dateCreated",
+      key: "dateCreated",
+    },
+  ];
+  return (
+    <div>
+      <Table loading={isLoading} dataSource={dataSource} columns={columns} />
+    </div>
+  );
 };
 
 export default RegisteredUsers;
