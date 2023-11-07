2216163b69f01ac1ca185018685c71f5c5b70049
russellbanares322 <banaresrussell11@gmail.com>
2023-11-07 20:49:46 +0800

    feat: remove created 404 html file

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="pet-adoption-system">
+    <BrowserRouter>
       <App />
       <ToastContainer
         position="bottom-right"
