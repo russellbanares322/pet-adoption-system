42047017069e04cb9b4babfdb6094f87a0282c6c
russellbanares322 <banaresrussell11@gmail.com>
2024-01-03 20:43:08 +0800

    feat: use user email from local storage instead of useAuthState hook

--- src/api/favorites/favorites.ts
+++ src/api/favorites/favorites.ts
@@ -1,17 +1,16 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, db } from "../../firebase/firebase-config";
+import {  db } from "../../firebase/firebase-config";
+import useUserInfo from "../../hooks/useUserInfo";
 import { PetsData } from "../pets/pets";
 
 const useFetchFavoritePosts = () => {
     const [data, setData] = useState<PetsData[]>([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [user] = useAuthState(auth);
-  
+    const {email} = useUserInfo()
     const getFavoritePosts = () => {
       setIsLoading(true);
-      const userSavedPosts = doc(db, "users", user?.email + "");
+      const userSavedPosts = doc(db, "users", email + "");
 
       onSnapshot(userSavedPosts, (doc) => {
         setData(doc.data()?.savedFavoritePets)
@@ -21,9 +20,9 @@ const useFetchFavoritePosts = () => {
   
     useEffect(() => {
       getFavoritePosts();
-    }, []);
+    }, [email]);
   
     return { data, isLoading };
   };
 
-  export {useFetchFavoritePosts}
+  export { useFetchFavoritePosts }
