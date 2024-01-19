a84b5cc5343c40212b7fbab4b5ffae4e75c26f97
russellbanares322 <banaresrussell11@gmail.com>
2024-01-19 20:19:01 +0800

    feat: instantiate blogs component

---
+++ src/pages/blogs/Blogs.tsx
@@ -0,0 +1,12 @@
+const Blogs = () => {
+  return (
+    <div className="py-24 w-full bg-whitesmoke min-h-screen h-full">
+      <div className="container">
+        <h1 className="font-bold text-3xl text-maroon">Blogs</h1>
+        <div className="mt-4">Content here</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
