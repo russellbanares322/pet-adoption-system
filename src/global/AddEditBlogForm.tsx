cc84c2ee2e569e72a4dc8d4e5f3654ecb6d10860
russellbanares322 <banaresrussell11@gmail.com>
2024-01-19 21:01:54 +0800

    feat: create state for opening blog form modal

---
+++ src/global/AddEditBlogForm.tsx
@@ -0,0 +1,11 @@
+import { Form } from "antd";
+
+const AddEditBlogForm = () => {
+  return (
+    <Form name="add-blog" initialValues={{ remember: true }}>
+      AddEditBlogForm
+    </Form>
+  );
+};
+
+export default AddEditBlogForm;
--- src/pages/blogs/Blogs.tsx
+++ src/pages/blogs/Blogs.tsx
@@ -1,10 +1,44 @@
+import { Modal } from "antd";
+import { useState } from "react";
+import AddEditBlogForm from "../../global/AddEditBlogForm";
+
 const Blogs = () => {
+  const [openBlogFormModal, setOpenBlogFormModal] = useState(false);
+
+  const handleToggleBlogFormModal = () => {
+    setOpenBlogFormModal(!openBlogFormModal);
+  };
+
+  const handleCloseBlogFormModal = () => {
+    setOpenBlogFormModal(false);
+  };
+
   return (
     <div className="py-24 w-full bg-whitesmoke min-h-screen h-full">
       <div className="container">
-        <h1 className="font-bold text-3xl text-maroon">Blogs</h1>
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="font-bold text-3xl text-maroon">Blogs</h1>
+            <p>
+              Explore inspiring stories of everyday champions saving stray pets.
+            </p>
+          </div>
+          <button onClick={handleToggleBlogFormModal} className="button-filled">
+            Add Story
+          </button>
+        </div>
         <div className="mt-4">Content here</div>
       </div>
+      <Modal
+        footer={null}
+        closable
+        width={500}
+        open={openBlogFormModal}
+        onCancel={handleCloseBlogFormModal}
+        title="ADD BLOG"
+      >
+        <AddEditBlogForm />
+      </Modal>
     </div>
   );
 };
